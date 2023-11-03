51e815ead82c0a72e80b35ebcd4a542f293153fd
Adryannunes <adryanhenrique62@gmail.com>
2023-11-03 16:56:05 -0300

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 (4.months.ago.to_date..Date.current).each do |date| 
   rand(6).times do 
    Despesa.create!(date: date, description: Faker::Commerce.product_name,
-     type: Despesa.types.keys.sample, quantia: Faker::Number.between(from: 10.00, to: 500.00))
+     type: Despesa.types.keys.sample, quantia: Faker::Number.between(from: 10.00, to: 50.00))
   end 
 end 
